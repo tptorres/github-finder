b086719c483c87f9ec3f4ee34db36c70d38c66f2
Tyler <tptorres5@gmail.com>
2019-10-03 19:08:13 -0400

    FIrst commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
+import Search from './components/users/Search';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  /* async componentDidMount() {
+    console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET);
+
+    this.setState({ loading: true });
+    const response = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
+      ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: response.data, loading: false });
+  } */
+
+  searchUsers = async text => {
+    this.setState({ loading: true });
+    const response = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
+      ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: response.data.items, loading: false });
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+        <div className='container'>
+          <Search searchUsers={this.searchUsers} />
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
